83aa140d4785d1bd9e942637acb50408f7351f20
zhCLT <zeeshan.hameed@crewlogix.com>
2025-12-10 18:39:24 +0500

    render new members html from database

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 # Create your views here.
 
 def member(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers':mymembers,
+    }
+    return HttpResponse(template.render(context,request))
